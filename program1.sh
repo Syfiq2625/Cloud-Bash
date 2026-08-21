9195977485b444223baeb3d4fbbcd655226f25bc
Syfiq2625 <syafiqdawwas3@gmail.com>
2026-08-21 13:27:25 +0000

    Ini adalah program pertama saya

---
+++ program1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ini adalah program awal saya
+waktu=$(LC_TIME=id_ID.UTF-8 date "+%A-%d-%m-%Y")
+
+
+echo -e "\n"
+echo "=====     PROFIL_SAYA     ====="
+echo "-------------------------------"
+echo -e "Hallo nama saya\t\t: Syafiq"
+echo -e "Dengan User\t\t: $(whoami)"
+echo -e "File ini dibuat pada\t: $waktu"
+
+echo -e "\n"
